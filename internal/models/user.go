25bb33a1227987a672b7569e2d10c8fbed50ef5a
Александр Халецкий <khaletskiy_ai@apteka-aprel.ru>
2022-06-24 17:09:27 +0300

    delete and get_friends methods works

--- internal/models/user.go
+++ internal/models/user.go
@@ -1,10 +1,12 @@
 package model
 
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
 type User struct {
-	ID      string `bson:"id,omitempty"`
-	Name    string `json:"name"`
-	Age     int    `json:"age"`
-	Friends []string
+	ID      primitive.ObjectID   `bson:"_id,omitempty"`
+	Name    string               `bson:"name"`
+	Age     int                  `bson:"age"`
+	Friends []primitive.ObjectID `bson:"friends"`
 }
 
 func (u *User) RemoveFromFriends(user *User) {
